4ae0cc3561ffdea94eb6ab76d7cb9c56c0a4021b
janZ <janzander96@gmail.com>
2024-10-02 08:47:34 +0800

    edited route for .get

--- routes/swagger.js
+++ routes/swagger.js
@@ -3,6 +3,6 @@ const swaggerUI = require("swagger-ui-express");
 const swaggerDocument = require("../swagger.json");
 
 router.use("/api-docs", swaggerUI.serve);
-router.get("api-docs", swaggerUI.setup(swaggerDocument));
+router.get("/api-docs", swaggerUI.setup(swaggerDocument));
 
 module.exports = router;
