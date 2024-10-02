9203e75284f35e83adea05f6685fea1c2a2c3e3b
janZ <janzander96@gmail.com>
2024-10-02 08:39:06 +0800

    added swagger api doc

--- routes/types.js
+++ routes/types.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { check, query, param, validationResult } = require('express-validator');
 
 const typesController = require("../controllers/typesController");
 
